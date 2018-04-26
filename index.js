233eca8e112917c4b721b2e644828155e8c1b7ec
ChriswFoster <chriswfoster@gmail.com>
2018-04-26 09:50:01 -0500

    Building index.js

--- index.js
+++ index.js
@@ -13,10 +13,20 @@ const writeToFile = data => {
 
 // scraping is always asynchronous.
 ;(async () => {
+  const data = []
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage() // sort of like opening up a new tab.
   await page.goto("https://en.wikipedia.org/wiki/Main_Page")
   await sleep(page, 60000)
-  const randomArticleLink = document.getElementById("n-randompage")
+  //can't use .map, for each, etc. They're not blocking and can't access this data. So use regular for loop
+  for (let i = 0; i < 10; i++) {
+    await page.click("#n-randompage") //targets element's id and clicks it.
+    const paragraphs = await page.$$("#content p")
+    for (let j = 0; j < paragraphs.length; j++) {
+      data.push({
+        p: await (await paragraphs[j].getProperty("innerText")).jsonValue()
+      })
+    }
+  }
 })()
 //  ^^^^  this is called an immediately invoked expression. As soon as file loads, it fires off immediately. The same is creating a function, then following it with an invocation.
